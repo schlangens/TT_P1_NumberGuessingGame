d8854344e136952dbb82fb5b34b5829cc36b8e76
Scott Schlangen <sschlangen@gmail.com>
2021-09-01 23:28:24 -0400

    Update guessing_game.py
    Added error handling for if not valid type

--- guessing_game.py
+++ guessing_game.py
@@ -16,17 +16,21 @@ def start_game():
     print("---------------------------------------------------------------------------------------------")
     print("Welcome to the number guessing game! {} Guess a number between 1 - 10  ".format(name))
     while attempts < attempt_limit:
-      guess_text = input("What is your best guess? ")
-      guess_text = int(guess_text)
-      attempts += 1
-      
-      if answer == guess_text:
-        print(f"You guess the right number! it was {guess_text}. It took you {attempts} attempt(s) to guess the right number!")
-        break
-      elif guess_text < answer:
-        print("It's higher.")
+      try:
+          guess_text = input("What is your best guess? ")
+          guess_text = int(guess_text)
+          attempts += 1
+      except ValueError as err:
+              print("is not a valid number 1-10".format(err))
+              continue
       else:
-        print("It's lower")
+          if answer == guess_text:
+            print(f"You guess the right number! it was {guess_text}. It took you {attempts} attempt(s) to guess the right number!")
+            break
+          if guess_text < answer:
+              print("It's higher.")
+          elif guess_text > answer:
+              print("It's lower")
     print("---------------------------------------------------------------------------------------------")    
     print(f"Bye {name}, you're done after those {attempts} attempts")
 
